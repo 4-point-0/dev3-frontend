6315d5581969d571d12fdc2dc6fdeae9573938ae
badurina <antun.badurina@gmail.com>
2022-12-19 09:53:10 +0100

    Add early exit to getLogoUrl

--- utils/logo.ts
+++ utils/logo.ts
@@ -2,6 +2,10 @@ import { File } from "../services/api/dev3Schemas";
 
 // Logo urls are not changed when the asset is patched,
 // so the updatedAt is added as an auxiliary query param to bust the browser image cache
-export function getLogoUrl(logo: File): string {
+export function getLogoUrl(logo: File) {
+  if (!logo) {
+    return;
+  }
+
   return `${logo.url}?u=${logo.updatedAt}`;
 }
