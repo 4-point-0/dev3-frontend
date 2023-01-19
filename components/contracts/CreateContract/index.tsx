c4d65fd5fc7ce5433023fbf27fe320f8eda4bdd3
Antun Badurina <antun.badurina@gmail.com>
2023-01-19 16:20:57 +0100

    Adjust create deployment request

--- components/contracts/CreateContract/index.tsx
+++ components/contracts/CreateContract/index.tsx
@@ -1,22 +1,27 @@
 import {
   Badge,
-  Button,
-  Card,
-  Grid,
+  Checkbox,
   Group,
+  Paper,
   Stack,
   Text,
+  TextInput,
   Title,
 } from "@mantine/core";
+import { useDebouncedValue } from "@mantine/hooks";
+import { DataTable, DataTableColumn } from "mantine-datatable";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
-import { ArrowLeft } from "tabler-icons-react";
+import { Search } from "tabler-icons-react";
+
 import { useSelectedProject } from "../../../context/SelectedProjectContext";
-import { fetchDeployedContractControllerCreate } from "../../../services/api/dev3Components";
+import { usePaginationProps } from "../../../hooks/usePaginationProps";
+import {
+  fetchDeployedContractControllerCreate,
+  useContractControllerFindAll,
+} from "../../../services/api/dev3Components";
 import { notifications } from "../../../utils/notifications";
 import { ContractForm, IContractFormValues } from "../ContractForm";
-
-import { TemplateList } from "../TemplateList";
 import { AuditedIcon } from "../TemplateList/AuditedIcon";
 
 export interface IContractTemplate {
@@ -28,107 +33,161 @@ export interface IContractTemplate {
   description: string;
 }
 
-export const CreateContract = () => {
+const PAGE_LIMIT = 20;
+
+export const CreateContract: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [auditedOnly, setAuditedOnly] = useState(false);
   const { projectId } = useSelectedProject();
   const router = useRouter();
   const [isCreating, setIsCreating] = useState(false);
-  const [contractTemplate, setContractTemplate] =
-    useState<IContractTemplate | null>(null);
 
-  const handleSelect = (template: IContractTemplate) => {
-    setContractTemplate(template);
+  const [debouncedQuery] = useDebouncedValue(query, 300);
+
+  const { data, isLoading } = useContractControllerFindAll({
+    queryParams: {
+      ...(auditedOnly && { isAudited: true }),
+      ...(debouncedQuery && { name: debouncedQuery }),
+      limit: PAGE_LIMIT,
+    },
+  });
+
+  const paginationProps = usePaginationProps({
+    page,
+    onPageChange: setPage,
+    limit: PAGE_LIMIT,
+    total: data?.total,
+  });
+
+  const handleQueryChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    setPage(1);
+    setQuery(event.currentTarget.value);
   };
 
-  const handleReset = () => {
-    setContractTemplate(null);
+  const handleAuditedOnlyChange = (
+    event: React.SyntheticEvent<HTMLInputElement>
+  ) => {
+    setPage(1);
+    setAuditedOnly(event.currentTarget.checked);
   };
 
-  const handleSumbit = async (values: IContractFormValues) => {
-    console.log(values);
-    if (!(projectId && contractTemplate)) {
-      return;
-    }
+  const handleSubmit = (templateId: string) => {
+    return async (values: IContractFormValues) => {
+      console.log(values);
+      if (!(projectId && templateId)) {
+        return;
+      }
 
-    setIsCreating(true);
+      setIsCreating(true);
 
-    try {
-      notifications.create({
-        title: "Creating contract deployment request",
-      });
+      try {
+        notifications.create({
+          title: "Creating contract deployment request",
+        });
 
-      await fetchDeployedContractControllerCreate({
-        body: {
-          alias: values.alias,
-          project_id: projectId,
-          contract_template_id: contractTemplate._id,
-          args: {},
-        },
-      });
+        await fetchDeployedContractControllerCreate({
+          body: {
+            alias: values.alias,
+            project_id: projectId,
+            contract_template_id: templateId,
+            args: {},
+          },
+        });
 
-      notifications.success({
-        title: "Created contract deployment request",
-      });
+        notifications.success({
+          title: "Created contract deployment request",
+        });
 
-      router.push("/contracts?tab=deploy");
-    } catch {
-      notifications.error({
-        title: `Failed to create contract deployment request`,
-      });
-    } finally {
-      setIsCreating(false);
-    }
+        router.push("/contracts?tab=deploy");
+      } catch {
+        notifications.error({
+          title: `Failed to create contract deployment request`,
+        });
+      } finally {
+        setIsCreating(false);
+      }
+    };
   };
+
+  const columns: Array<DataTableColumn<IContractTemplate>> = [
+    {
+      accessor: "name",
+      render: ({ name, is_audited, creator_name }) => {
+        return (
+          <Group noWrap>
+            <AuditedIcon isAudited={is_audited} />
+
+            <Stack spacing={0}>
+              <Text fz="xs" tt="uppercase">
+                {creator_name}
+              </Text>
+              <Title order={4}>{name}</Title>
+            </Stack>
+          </Group>
+        );
+      },
+    },
+    {
+      accessor: "description",
+      title: "",
+      render: ({ description, tags }) => {
+        return (
+          <Stack>
+            <Text>{description}</Text>
+            <Group spacing="xs">
+              {tags.map((tag) => (
+                <Badge key={tag}>{tag}</Badge>
+              ))}
+            </Group>
+          </Stack>
+        );
+      },
+    },
+  ];
 
   return (
-    <Stack>
-      {!contractTemplate && (
-        <>
-          <TemplateList onSelect={handleSelect} />
-        </>
-      )}
-      {contractTemplate && (
-        <>
-          <Group position="left">
-            <Button
-              variant="default"
-              leftIcon={<ArrowLeft size={14} />}
-              onClick={handleReset}
-            >
-              Back
-            </Button>
-          </Group>
+    <>
+      <Group>
+        <TextInput
+          sx={{ flex: 1 }}
+          placeholder="Search templates..."
+          icon={<Search size={16} />}
+          value={query}
+          onChange={handleQueryChange}
+          disabled={isCreating}
+        />
+        <Checkbox
+          label="Audited only"
+          checked={auditedOnly}
+          onChange={handleAuditedOnlyChange}
+          disabled={isCreating}
+        />
+      </Group>
 
-          <Grid>
-            <Grid.Col md={7}>
-              <Card shadow="sm" p="lg" withBorder>
-                <Stack>
-                  <Group>
-                    <AuditedIcon isAudited={contractTemplate.is_audited} />
-
-                    <Stack spacing={0}>
-                      <Text fz="xs" tt="uppercase">
-                        {contractTemplate.creator_name}
-                      </Text>
-                      <Title order={4}>{contractTemplate.name}</Title>
-                    </Stack>
-                  </Group>
-
-                  <Text>{contractTemplate.description}</Text>
-
-                  <Group spacing="xs">
-                    {contractTemplate.tags.map((tag) => (
-                      <Badge key={tag}>{tag}</Badge>
-                    ))}
-                  </Group>
-                </Stack>
-              </Card>
-            </Grid.Col>
-            <Grid.Col md={5}>
-              <ContractForm disabled={isCreating} handleSubmit={handleSumbit} />
-            </Grid.Col>
-          </Grid>
-        </>
-      )}
-    </Stack>
+      <DataTable
+        minHeight={200}
+        sx={{ thead: { display: "none" } }}
+        columns={columns}
+        records={data?.results}
+        fetching={isLoading}
+        idAccessor="_id"
+        emptyState="No templates found"
+        {...paginationProps}
+        rowExpansion={{
+          content: ({ record }) => {
+            return (
+              <Paper p="lg">
+                <Stack></Stack>
+                <ContractForm
+                  disabled={isCreating}
+                  handleSubmit={handleSubmit((record as IContractTemplate)._id)}
+                />
+              </Paper>
+            );
+          },
+        }}
+      />
+    </>
   );
 };
