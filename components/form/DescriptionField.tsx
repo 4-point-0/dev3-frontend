d14616652c72d644c4360900cc55910e24da5d09
badurina <antun.badurina@gmail.com>
2023-02-02 11:36:57 +0100

    Adjust description content

--- components/form/DescriptionField.tsx
+++ components/form/DescriptionField.tsx
@@ -12,12 +12,12 @@ export default function DescriptionField<
   S extends StrictRJSFSchema = RJSFSchema,
   F extends FormContextType = any
 >({ description, id }: DescriptionFieldProps<T, S, F>) {
+  if (typeof description === "string") {
+    return <Alert mb="md">{description.replace("Public method: ", "")}</Alert>;
+  }
+
   if (description) {
-    return (
-      <>
-        <Alert mb="md">{description}</Alert>
-      </>
-    );
+    return <Alert mb="md">{description}</Alert>;
   }
 
   return null;
